4387b919231a1b972da3fb6b8c04207d6ee68437
Coleman Sperando <coleman@Colemans-Air.lan>
2021-02-19 12:17:47 -0500

    pig latin app

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
-(function(window) {
+(function() {
 'use strict';
 
-  angular.module('testApp', [])
-    .controller('testAppController', testAppController);
+  angular.module('filterApp', [])
+    .controller('filterAppController', filterAppController)
+    .filter('pig', PigLatinFilterFactory)
+    .filter('custom', CustomFilterFactory);
 
-    testAppController.$inject = ['$scope', '$filter'];
+  // protect from minifier
+  filterAppController.$inject = ['$scope', 'customFilter', 'pigFilter'];
 
-    function testAppController($scope, $filter, $injector) {
-      $scope.string = "";
+  function filterAppController($scope, customFilter, pigFilter) {
+    $scope.string = "";
+    $scope.newString = "";
+    $scope.pigString = "";
 
-      $scope.sendMessage = function() {
-        return 'this is a message';
+    $scope.makeString = function() {
+      //console.log('test');
+      $scope.newString = customFilter($scope.string);
+    }
+
+    $scope.makePigString = function() {
+      $scope.pigString = pigFilter($scope.string);
+    }
+
+  }
+
+  function CustomFilterFactory() {
+    return function(input) {
+      input = input || ""; // catch missing input
+      return input.toUpperCase();
+    };
+  }
+
+  function PigLatinFilterFactory() {
+    return function(input) {
+      input = input || "";
+
+      var words = getWordsArray(input);
+      var pigWords = words;
+      for (var i = 0; i < words.length; i++) {
+        pigWords[i] = toPig(words[i]);
       }
 
-    };
+      input = pigWords.toString().replace(/,/g, ' ').toUpperCase();
 
-})(window);
+      return input;
+    };
+  }
+
+  function getWordsArray(string) {
+    var words = string.split(' ');
+    return words;
+  }
+
+  function toPig(string) {
+    string = string || "";
+    string = string.slice(1) + string[0] + 'ay';
+    return string;
+  }
+
+})();
